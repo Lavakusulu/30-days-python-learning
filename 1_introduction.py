d09d372f1186d7ff28d71a66b8827059022bfb5a
lavakusulu <lavakusulua@gmail.com>
2026-06-04 19:08:11 +0530

    day1 of my python learning

---
+++ 1_introduction.py
@@ -0,0 +1,29 @@
+#day1 introduction, installation,syntax,i/p and o/p
+
+#1 
+'''print("Hello world")'''
+
+#2
+"""n = input("Username: ")
+print("Welcome",n)"""
+
+#3
+"""a,b,c =map(int, input().split())
+sum = a+b+c
+print(sum)"""
+
+#4
+"""a,b =map(int,input().split())
+a,b=b,a
+print("a=",a,"b=",b)"""
+
+#5
+"""n = "Hello world"
+print(type(n))"""
+
+#task even or odd number
+"""n = int(input("enter a number:"))
+if n%2 == 0:
+    print("even")
+else:
+    print("odd")"""
