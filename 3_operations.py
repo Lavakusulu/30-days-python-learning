f6a8ac3dba624277d82240d2c2c3ced0e5f4034f
lavakusulu <lavakusulua@gmail.com>
2026-06-08 15:10:17 +0530

    my day 3 topic is operation in python

---
+++ 3_operations.py
@@ -0,0 +1,34 @@
+#finding area of circle
+"""n = int(input())
+pi = 3.141
+area= pi*(n**2)
+print(area)"""
+
+#checking the number is multiple of given number or not
+"""m= int(input("Enter a number if you want to check: "))
+if m%num==0:
+    print("it is multiple of",num)
+else:
+    print("not division of",num)"""
+
+#Evaluating the expression
+"""n = input()
+print(eval(n))"""
+
+#Use membership operator on list/string. 
+"""n = input()
+m= input()
+if m in n:
+    print("found")
+else:
+    print("not found")"""
+
+#Check if two variables point to same object. 
+"""n = input()
+m= n
+if m is n:
+    print("found")
+else:
+    print("not found")"""
+
+#STARE
