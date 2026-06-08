9a309465cc998b50300b274729dd7c5e3a41b980
lavakusulu <lavakusulua@gmail.com>
2026-06-08 15:33:03 +0530

    day1

--- 1_introduction.py
+++ 1_introduction.py
@@ -1,23 +1,23 @@
 #day1 introduction, installation,syntax,i/p and o/p
 
-#1 
+#1 print statement
 '''print("Hello world")'''
 
-#2
+#2print username and welcome
 """n = input("Username: ")
 print("Welcome",n)"""
 
-#3
+#3print sum of 3 numbers
 """a,b,c =map(int, input().split())
 sum = a+b+c
 print(sum)"""
 
-#4
+#4swap two numbers
 """a,b =map(int,input().split())
 a,b=b,a
 print("a=",a,"b=",b)"""
 
-#5
+#5print type of a variable
 """n = "Hello world"
 print(type(n))"""
 
